6e95c70c409feaf1fa2d4ebe01281c143b141c52
Patrick Eschenfeldt <peschenfeldt@mgh-ita.org>
2018-11-15 22:13:22 -0500

    Optional multiprocessing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import itertools
 import collections
+import multiprocessing as mp
 import data_io
 from stroke.patient import Patient
 import stroke.stroke_model as sm
@@ -31,7 +32,8 @@ def results_name(times_file, hospitals_file, fix_performance,
 
 
 def run_model(times_file, hospitals_file, fix_performance=False,
-              patient_count=100, simulation_count=1000, **kwargs):
+              patient_count=100, simulation_count=1000, cores=None,
+              **kwargs):
     '''Run the model on the given map points for the given hospitals. The
         times file should be in data/travel_times and contain travel times to
         appropriate hospitals. The hospitals file should be in data/hospitals
@@ -52,35 +54,65 @@ def run_model(times_file, hospitals_file, fix_performance=False,
                             simulation_count)
     first_pat_num = data_io.get_next_patient_number(res_name)
     
+    if cores is False:
+        pool = False
+    else:
+        pool = mp.Pool(cores)
+    
     for pat_num, patient in enumerate(tqdm(patients, desc='Patients')):
         patient_results = []
         for point, these_times in tqdm(times.items(), desc='Map Points', leave=False):
             for uses_hospital_performance, hospital_list in hospital_lists:
-                model = sm.StrokeModel(patient, hospital_list)
-                model.set_times(these_times)
-                these_results = model.run(
-                    n=simulation_count,
-                    fix_performance=fix_performance
-                )
-                results = collections.OrderedDict()
-                results['Location'] = point
-                results['Patient'] = first_pat_num + pat_num
-                results['Varying Hospitals'] = uses_hospital_performance
-                results['Primary Count'] = len(model.primaries)
-                results['Comprehensive Count'] = len(model.comprehensives)
-                results['Sex'] = str(patient.sex)
-                results['Age'] = patient.age
-                results['Symptoms'] = patient.symptom_time
-                results['RACE'] = patient.severity.score
-                cbc = these_results.counts_by_center
-                cbc = {str(center): count for center, count in cbc.items()}
-                results.update(cbc)
-                
+                if pool:
+                    results = pool.apply_async(
+                        run_one_scenario,
+                        (patient, point, these_times, hospital_list,
+                         uses_hospital_performance,
+                         simulation_count, fix_performance,
+                         first_pat_num, pat_num)
+                    )
+                else:
+                    results = run_one_scenario(
+                        patient, point, these_times, hospital_list,
+                        uses_hospital_performance,
+                        simulation_count, fix_performance,
+                        first_pat_num, pat_num
+                    )
                 patient_results.append(results)
+        if pool:
+            to_fetch = tqdm(patient_results, desc='Map Points', leave=False)
+            patient_results = [job.get() for job in to_fetch]
         # Save after each patient in case we cancel or crash
         data_io.save_patient(res_name, patient_results, hospitals)
 
     return
+
+
+def run_one_scenario(patient, point, these_times, hospital_list,
+                     uses_hospital_performance,
+                     simulation_count, fix_performance,
+                     first_pat_num, pat_num):
+    model = sm.StrokeModel(patient, hospital_list)
+    model.set_times(these_times)
+    these_results = model.run(
+        n=simulation_count,
+        fix_performance=fix_performance
+    )
+    results = collections.OrderedDict()
+    results['Location'] = point
+    results['Patient'] = first_pat_num + pat_num
+    results['Varying Hospitals'] = uses_hospital_performance
+    results['Primary Count'] = len(model.primaries)
+    results['Comprehensive Count'] = len(model.comprehensives)
+    results['Sex'] = str(patient.sex)
+    results['Age'] = patient.age
+    results['Symptoms'] = patient.symptom_time
+    results['RACE'] = patient.severity.score
+    cbc = these_results.counts_by_center
+    cbc = {str(center): count for center, count in cbc.items()}
+    results.update(cbc)
+    
+    return results
 
 
 def main(args):
@@ -88,9 +120,14 @@ def main(args):
     hospitals_file = args.hospital_file
     patient_count = args.patients
     simulation_count = args.simulations
+    if args.multicore:
+        cores = None
+    else:
+        cores = False
 
     run_model(times_file, hospitals_file, patient_count=patient_count,
-              fix_performance=False, simulation_count=simulation_count)
+              fix_performance=False, simulation_count=simulation_count,
+              cores=cores)
 
 
 if __name__ == '__main__':
@@ -111,5 +148,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '-s', '--simulations', type=int, default=1000, help=s_help
     )
+    parser.add_argument('-m', '--multicore', action='store_true',
+                        help='Use all available CPU cores')
     args = parser.parse_args()
     main(args)
