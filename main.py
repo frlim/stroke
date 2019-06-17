1c3e2e0bcd2bff3975c4255b7d7ef557580de2df
Truong <hqt2102@cumc.columbia.edu>
2019-06-17 16:52:15 -0400

    Provide option to run default DTN or Kori's DTN in main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def results_name(base_dir, times_file, hospitals_file, fix_performance,
     out_name = f'times={times_file.strip(".csv")}'
     out_name += f'_hospitals={hospitals_file.strip(".csv")}'
     out_name += '_fixed' if fix_performance else '_random'
-    out_name += '_male' if sex==0 else '_female'
-    out_name += '_python.csv'
+    out_name += '_'+str(sex)
+    out_name += '.csv'
     out_dir = os.path.join(base_dir, 'output')
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
@@ -99,6 +99,72 @@ def run_model(
     if pool:
         pool.close()
     return
+
+def run_model_defaul_dtn(
+        times_file,
+        hospitals_file,
+        fix_performance=False,
+        patient_count=10,
+        simulation_count=1000,
+        cores=None,
+        base_dir='',  # default: current working directory
+        locations=None,  # default: run for all location in times_file
+        **kwargs):
+    '''Run the model on the given map points for the given hospitals. The
+        times file should be in data/travel_times and contain travel times to
+        appropriate hospitals. The hospitals file should be in data/hospitals
+        and contain transfer destinations and times for all primary hospitals.
+        kwargs -- passed through to inputs.Inputs.random to hold parameters
+                    constant
+        This method use travel_time file generated from hospital list Kori gave
+        but instead of using DTN times from AHA, we use default_times generated
+        from a uniform distribution
+        for now, use fix_performance = True to make it fair for the hospitals
+    '''
+    hospitals = data_io.get_hospitals(hospitals_file,use_default_times=True)
+    hospital_lists = [(False, hospitals)]
+
+    patients = [Patient.random(**kwargs) for _ in range(patient_count)]
+    sex = patients[0].sex
+
+    times = data_io.get_times_real_data(times_file)
+    if locations:  # Not none
+        times = {loc: time for loc, time in times.items() if loc in locations}
+
+    res_name = results_name(base_dir, times_file, hospitals_file,
+                            fix_performance, simulation_count, sex)
+    first_pat_num = data_io.get_next_patient_number(res_name)
+
+    if cores is False:
+        pool = False
+    else:
+        pool = mp.Pool(cores)
+    for pat_num, patient in enumerate(tqdm(patients, desc='Patients')):
+        patient_results = []
+        for point, these_times in tqdm(
+                times.items(), desc='Map Points', leave=False):
+            for uses_hospital_performance, hospital_list in hospital_lists:
+                if pool:
+                    results = pool.apply_async(
+                        run_one_scenario,
+                        (patient, point, these_times, hospital_list,
+                         uses_hospital_performance, simulation_count,
+                         fix_performance, first_pat_num, pat_num))
+                else:
+                    results = run_one_scenario(
+                        patient, point, these_times, hospital_list,
+                        uses_hospital_performance, simulation_count,
+                        fix_performance, first_pat_num, pat_num)
+                patient_results.append(results)
+        if pool:
+            to_fetch = tqdm(patient_results, desc='Map Points', leave=False)
+            patient_results = [job.get() for job in to_fetch]
+        # Save after each patient in case we cancel or crash
+        data_io.save_patient(res_name, patient_results, hospitals)
+    if pool:
+        pool.close()
+    return
+
 
 
 DTN_FILE = Path('Z:\\stroke_data')/'deidentified_DTN.xlsx'
@@ -240,6 +306,39 @@ def main(args):
         locations=locations,
         **kwargs)
 
+def main_default_dtn(args):
+    times_file = args.times_file
+    hospitals_file = args.hospital_file
+    patient_count = args.patients
+    simulation_count = args.simulations
+    kwargs = parse_extra_inputs(args)
+
+    # if args.base_dir:
+    #     base_dir = args.base_dir  # dir to put output file in
+    # else:
+    #     base_dir = ''  # default: current working directory
+    base_dir=''
+
+    if hasattr(args, 'locations'):
+        locations = args.locations
+    else:
+        locations = None
+
+    if args.multicore:
+        cores = None
+    else:
+        cores = False
+
+    run_model_defaul_dtn(
+        times_file,
+        hospitals_file,
+        patient_count=patient_count,
+        fix_performance=False,
+        simulation_count=simulation_count,
+        cores=cores,
+        base_dir=base_dir,
+        locations=locations,
+        **kwargs)
 
 if __name__ == '__main__':
     p_default = 2
--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -6,23 +6,39 @@ from stroke import constants
 
 if __name__ == '__main__':
     # change to data
-    hospital_path = str(Path('data/hospitals/Demo.csv'))
-    times_path = str(Path('data/travel_times/Demo.csv'))
+    hospital_path = str(Path('data/hospitals/MA_n=100.csv'))
+    times_path = str(Path('data/travel_times/MA_n=100.csv'))
 
-    sex = constants.Sex.FEMALE
-    upper = 1
+    sex = constants.Sex.MALE
+    age=70
+    race=8
+    time_since_symptoms=50
     s_default = 1000
-    for age in range(70, 70 + upper, 5):  # 30 to 85
-        for race in range(7, 7 + upper, 1):  # 0 to 9
-            for time_since_symptoms in range(50, 100 + upper, 10):  # 10 to 100
-                args = Namespace(
-                    patients=1,
-                    simulations=s_default,
-                    multicore=True,
-                    hospital_file=str(hospital_path),
-                    times_file=str(times_path),
-                    sex=sex,
-                    age=age,
-                    race=race,
-                    time_since_symptoms=time_since_symptoms)
-                main.main(args)
+    args = Namespace(
+        patients=1,
+        simulations=s_default,
+        multicore=True,
+        hospital_file=str(hospital_path),
+        times_file=str(times_path),
+        sex=sex,
+        age=age,
+        race=race,
+        time_since_symptoms=time_since_symptoms)
+    main.main(args)
+
+    # upper = 1
+    #
+    # for age in range(70, 70 + upper, 5):  # 30 to 85
+    #     for race in range(7, 7 + upper, 1):  # 0 to 9
+    #         for time_since_symptoms in range(50, 100 + upper, 10):  # 10 to 100
+    #             args = Namespace(
+    #                 patients=1,
+    #                 simulations=s_default,
+    #                 multicore=True,
+    #                 hospital_file=str(hospital_path),
+    #                 times_file=str(times_path),
+    #                 sex=sex,
+    #                 age=age,
+    #                 race=race,
+    #                 time_since_symptoms=time_since_symptoms)
+    #             main.main(args)
