05ab599ac1868d7fb82c25cbe71ad0315b84b232
Truong <hqt2102@cumc.columbia.edu>
2019-07-02 16:30:15 -0400

    clean up code for easy reading

--- sensitivity_analysis_demo.py
+++ sensitivity_analysis_demo.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import main
 from argparse import Namespace
 from stroke import constants
-import os
 import paths
 from types import SimpleNamespace
 
@@ -18,8 +17,8 @@ SYMP_MAX = 100
 if __name__ == '__main__':
     # get file paths base on OS
     res_name_prefix = paths.RES_NAME_PREFIX
-    hospital_path = paths.HOSPITAL_PATH
-    times_path = paths.TIMES_PATH
+    hospital_path =Path('data/hospitals/Demo.csv')
+    times_path = Path('data/travel_times/Demo.csv')
     #s_default - 'auto' for automatic mode or else enter an integer
     s_default = 'auto'
     upper = 1
@@ -64,34 +63,20 @@ if __name__ == '__main__':
                     sex_str = 'male' if sex==constants.Sex.MALE else 'female'
                     res_name=str(res_name_prefix/
                     f'times={times_path.stem}_hospitals={hospital_path.stem}_sex={sex_str}_age={age}_race={race}_symptom={time_since_symptoms}_nsim={s_default}_beAHA.csv')
-                    args = Namespace(
-                        patients=1,
-                        simulations=s_default,
-                        multicore=True,
-                        hospital_file=str(hospital_path),
-                        times_file=str(times_path),
-                        sex=sex,
-                        age=age,
-                        race=race,
-                        time_since_symptoms=time_since_symptoms,
-                        res_name=res_name)
-                    main.main_default_dtn(args)
-
-                    res_name=str(res_name_prefix/
-                    f'times={times_path.stem}_hospitals={hospital_path.stem}_sex={sex_str}_age={age}_race={race}_symptom={time_since_symptoms}_nsim={s_default}_afAHA.csv')
-                    args = Namespace(
-                        patients=1,
-                        simulations=s_default,
-                        multicore=True,
-                        hospital_file=str(hospital_path),
-                        times_file=str(times_path),
-                        sex=sex,
-                        age=age,
-                        race=race,
-                        time_since_symptoms=time_since_symptoms,
-                        res_name=res_name)
-                    main.main(args)
-
+                    kwargs = {}
+                    kwargs['sex'] = sex
+                    kwargs['age'] = age
+                    kwargs['race'] = race
+                    kwargs['time_since_symptoms'] = time_since_symptoms
+                    main.run_model(times_file=times_path,
+                    hospitals_file=hospital_path,
+                    fix_performance=False,
+                    patient_count=1,
+                    simulation_count=s_default,
+                    cores=None, # use multicore if None
+                    res_name=res_name,
+                    **kwargs
+                    )
                     # if in resume modecheck off that resuming is done once loop finished
                     if resume_parameters.symp_resume & (time_since_symptoms==resume_parameters.symp):
                          resume_parameters.symp_resume = False
