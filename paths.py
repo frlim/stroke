1c90f64baaafa4fc4f8bd04857a320332c6c9ed6
Truong <hqt2102@cumc.columbia.edu>
2019-07-02 13:43:08 -0400

    minor fix typo

--- paths.py
+++ paths.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 if os.name=='nt': #LOCAL
     DTN_FILE = Path('Z:\\stroke_data')/'deidentified_DTN.xlsx'
-    RES_NAME_PREFIX = Path('E:/stroke_model_output/output_070219')
+    RES_NAME_PREFIX = Path('E:/stroke_model_output/output_070119')
     HOSPITAL_PATH =Path('data/hospitals/MA_n=100.csv')
     TIMES_PATH = Path('data/travel_times/MA_n=100.csv')
 else: # FOR TED
     _data_path = Path('/home/hqt2102/deidentified_stroke_data')
     DTN_FILE = _data_path/'deidentified_DTN_uc.xlsx'
-    RES_NAME_PREFIX  = _data_path/'output/output_070219'
+    RES_NAME_PREFIX  = _data_path/'output/output_070119'
     HOSPITAL_PATH = _data_path/'hospitals/MA_n=100.csv'
     TIMES_PATH = _data_path/'travel_times/MA_n=100.csv'
 
